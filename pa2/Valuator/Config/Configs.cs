fde91f851f18cbd7ec1d281148191d45a48a6b8d
Alexander <103064882+techman32@users.noreply.github.com>
2024-10-15 23:13:01 +0300

    Лабораторная работа 2

---
+++ pa2/Valuator/Config/Configs.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Valuator.Config;
+
+public static class Configs
+{
+    public static string REDIS_HOSTNAME = "localhost";
+    public static int REDIS_PORT = 6379;
+}
---
+++ pa2/scripts/end.cmd
@@ -0,0 +1,4 @@
+taskkill /f /im valuator.exe
+
+cd %~dp0..\nginx\
+taskkill /f /im nginx.exe
